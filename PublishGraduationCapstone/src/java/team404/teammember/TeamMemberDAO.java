3a6802f5068bd8e7d80f0d19414ca9c199273437
DatTranLK <thanhdat25062001@gmail.com>
2022-03-05 21:36:24 +0700

    Dat: add show sharepost and sharepostdetail api

--- PublishGraduationCapstone/src/java/team404/teammember/TeamMemberDAO.java
+++ PublishGraduationCapstone/src/java/team404/teammember/TeamMemberDAO.java
@@ -77,4 +77,54 @@ public class TeamMemberDAO {
 
         return null;
     }
+    public TeamMemberDTO getInforMember (String studentId)
+    {
+        try{
+            con = DBHelpers.makeConnection();
+            if(con != null)
+            {
+                String sql = "Select * "
+                        + "From TeamMember "
+                        + "Where StudentId = ? ";
+                stm = con.prepareStatement(sql);
+                stm.setString(1, studentId);
+                rs = stm.executeQuery();
+                if(rs.next())
+                {
+                    String studentId2 = rs.getString("StudentId");
+                    String memberName = rs.getNString("MemberName");
+                    String memberAvatar = rs.getString("MemberAvatar");
+                    String phone = rs.getString("Phone");
+                    String backupEmail = rs.getNString("BackupEmail");
+                    
+                    TeamMemberDTO dto = new TeamMemberDTO();
+                    dto.setMemberId(studentId2);
+                    dto.setMemberName(memberName);
+                    dto.setMemberAvatar(memberAvatar);
+                    dto.setPhone(phone);
+                    dto.setBackupEmail(backupEmail);
+                    return dto;
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(TeamMemberDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(TeamMemberDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stm != null) {
+                    stm.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(TeamMemberDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return null;
+    }
 }
