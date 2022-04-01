f83222b2d1151cf24a1d1144adbf18974ea852f8
dq-qiji <nguyendaoducquan@gmail.com>
2022-04-02 00:22:01 +0700

    Quan - addLogin

--- PublishGraduationCapstone/web/js/Admin_Main_Change.js
+++ PublishGraduationCapstone/web/js/Admin_Main_Change.js
@@ -1,3 +1,28 @@
+function ShowLogout() {
+    var logOut = document.getElementById('user-main-small-control');
+    if (logOut.style.display == 'none') {
+        logOut.style.display = 'block';
+    } else
+        logOut.style.display = 'none';
+}
+
+function logout() {
+    localStorage.clear();
+
+}
+
+function initLogin() {
+    var username = localStorage.getItem("name")
+    var picture = localStorage.getItem("picture")
+
+    document.getElementById("admin-main-nav-login-img-container").innerHTML =
+        `
+        <img id="admin-main-nav-login-img" src="${picture}" alt="">
+        `
+    document.getElementById("admin-main-nav-login").innerHTML = username
+    document.getElementById("user-main-small-name").innerHTML = username
+}
+
 function sendUserData(jsonData) {
     var xhr = new XMLHttpRequest();
     var api = "/PublishGraduationCapstone/api/admin/changeAccountList";
